461cd2a0cfa71511af131ee05687824688009c10
regnfylt <148742788+regnfylt@users.noreply.github.com>
2024-03-28 15:03:13 +0200

    Added example product

--- src/main/java/k24/op1/dogbackend/DogbackendApplication.java
+++ src/main/java/k24/op1/dogbackend/DogbackendApplication.java
@@ -1,14 +1,27 @@
 package k24.op1.dogbackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import k24.op1.dogbackend.domain.Product;
+import k24.op1.dogbackend.domain.ProductRepository;
 
 @SpringBootApplication
 public class DogbackendApplication {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(DogbackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(ProductRepository productRepository) {
+		return (args) -> {
+
+		productRepository.save(new Product("Ulkoilutakki", "Ulkoiluun soveltuva takki", "Varuste", "Multicolor", "Onesize", 50.00, "Rukka"));
+		};
 	}
 
 }
