eb26c20b8e7216e38f4d94f41ccc86ffa4d1a5a8
Rasmusbhd <bhd466@myy.haaga-helia.fi>
2024-03-28 12:25:32 +0200

    first commit

---
+++ src/main/java/k24/op1/dogbackend/DogbackendApplication.java
@@ -0,0 +1,13 @@
+package k24.op1.dogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/k24/op1/dogbackend/DogbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package k24.op1.dogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
