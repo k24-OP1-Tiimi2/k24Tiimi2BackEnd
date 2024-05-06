b1ddc7e91278fdde18bb6ce5020dac0382939bad
regnfylt <148742788+regnfylt@users.noreply.github.com>
2024-05-06 14:25:32 +0300

    Sori Jasu rip Postgres (tiimin päätös)

--- src/main/java/k24/op1/dogbackend/DogbackendApplication.java
+++ src/main/java/k24/op1/dogbackend/DogbackendApplication.java
@@ -1,16 +1,18 @@
 package k24.op1.dogbackend;
-/*
- *import org.springframework.boot.CommandLineRunner;
+
+import org.springframework.boot.CommandLineRunner;
 
 import org.springframework.context.annotation.Bean;
 
 import k24.op1.dogbackend.domain.Manufacturer;
 import k24.op1.dogbackend.domain.ManufacturerRepository;
+import k24.op1.dogbackend.domain.Order;
+import k24.op1.dogbackend.domain.OrderRepository;
 import k24.op1.dogbackend.domain.Product;
 import k24.op1.dogbackend.domain.ProductRepository;
 import k24.op1.dogbackend.domain.Type;
 import k24.op1.dogbackend.domain.TypeRepository; 
- */
+
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -22,14 +24,12 @@ import org.slf4j.LoggerFactory;
 import k24.op1.dogbackend.domain.ManufacturerRepository;
 import k24.op1.dogbackend.domain.ProductRepository;
 import k24.op1.dogbackend.domain.TypeRepository;
-import k24.op1.dogbackend.domain.AppUserRepository;
-
-/*Huom. Kommentoitu osia pois
 import k24.op1.dogbackend.domain.AppUser;
+import k24.op1.dogbackend.domain.AppUserRepository;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-*/
+
 @SpringBootApplication
 public class DogbackendApplication {
 	private static final Logger log = LoggerFactory.getLogger(DogbackendApplication.class);
@@ -40,54 +40,54 @@ public class DogbackendApplication {
 	}
 
 	@Bean
-	public CommandLineRunner initData(ProductRepository productRepository,
+	CommandLineRunner initData(ProductRepository productRepository,
 			ManufacturerRepository manufacturerRepository, TypeRepository typeRepository,
-			AppUserRepository auRepository) {
+			AppUserRepository auRepository, OrderRepository orderRepository) {
 
 		return (args) -> {
-			/*
-			 * Manufacturer rukka = new Manufacturer("Rukka");
-			 * manufacturerRepository.save(rukka);
-			 * 
-			 * Manufacturer martta = new Manufacturer("Martta");
-			 * manufacturerRepository.save(martta);
-			 * 
-			 * Manufacturer pomppa = new Manufacturer("Pomppa");
-			 * manufacturerRepository.save(pomppa);
-			 * 
-			 * Manufacturer hurtta = new Manufacturer("Hurtta");
-			 * manufacturerRepository.save(hurtta);
-			 * 
-			 * Type vaate = new Type("Vaate");
-			 * typeRepository.save(vaate);
-			 * Type lelu = new Type("Lelu");
-			 * typeRepository.save(lelu);
-			 * 
-			 * Product p1 = new Product("Ulkoilutakki", "Sininen", "S", 15.00, 25, vaate,
-			 * rukka);
-			 * productRepository.save(p1);
-			 * Product p2 = new Product("Vinkulelu", "Keltainen", "S", 5.00, 25, lelu,
-			 * martta);
-			 * productRepository.save(p2);
-			 * Product p3 = new Product("Sadetakki", "Punainen", "M", 25.00, 100, vaate,
-			 * pomppa);
-			 * productRepository.save(p3);
-			 * Product p4 = new Product("T-paita", "Valkoinen", "M", 10.00, 50, vaate,
-			 * hurtta);
-			 * productRepository.save(p4);
-			 * Product p5 = new Product("Purulelu", "Sini-valkoinen", "M", 7.50, 50, lelu,
-			 * rukka);
-			 * productRepository.save(p5);
-			 *
-			 */
-			/*
-			 * if (auRepository.findByUsername("admin") == null) {
-			 * PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-			 * String encodedPassword = passwordEncoder.encode("admin");
-			 * AppUser admin = new AppUser("admin", encodedPassword, "ADMIN");
-			 * auRepository.save(admin);
-			 * }
-			 */
+			
+			 Manufacturer rukka = new Manufacturer("Rukka");
+			 manufacturerRepository.save(rukka);
+			  
+			 Manufacturer martta = new Manufacturer("Martta");
+			 manufacturerRepository.save(martta);
+			  
+			 Manufacturer pomppa = new Manufacturer("Pomppa");
+			 manufacturerRepository.save(pomppa);
+			  
+			 Manufacturer hurtta = new Manufacturer("Hurtta");
+			 manufacturerRepository.save(hurtta);
+			  
+			 Type vaate = new Type("Vaate");
+			 typeRepository.save(vaate);
+			 Type lelu = new Type("Lelu");
+			 typeRepository.save(lelu);
+			  
+			 Product p1 = new Product("Ulkoilutakki", "Sininen", "S", 15.00, 25, vaate,
+			 rukka);
+			 productRepository.save(p1);
+			 Product p2 = new Product("Vinkulelu", "Keltainen", "S", 5.00, 25, lelu,
+			 martta);
+			 productRepository.save(p2);
+			 Product p3 = new Product("Sadetakki", "Punainen", "M", 25.00, 100, vaate,
+			 pomppa);
+			 productRepository.save(p3);
+			 Product p4 = new Product("T-paita", "Valkoinen", "M", 10.00, 50, vaate,
+			 hurtta);
+			 productRepository.save(p4);
+			 Product p5 = new Product("Purulelu", "Sini-valkoinen", "M", 7.50, 50, lelu,
+			 rukka);
+			 productRepository.save(p5);
+
+			Order o1 = new Order("Amogus", "Amogus", "Amogus");
+			orderRepository.save(o1);
+
+			 if (auRepository.findByUsername("admin") == null) {
+			 PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+			 String encodedPassword = passwordEncoder.encode("admin");
+			 AppUser admin = new AppUser("admin", encodedPassword, "ADMIN");
+			 auRepository.save(admin);
+			 }
 		};
 	}
 }
--- src/main/java/k24/op1/dogbackend/WebSecurityConfig.java
+++ src/main/java/k24/op1/dogbackend/WebSecurityConfig.java
@@ -4,14 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
@@ -22,11 +28,14 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(antMatcher("/css/**")).permitAll() // Enable CSS when logged out
                         .requestMatchers("/dogstore").permitAll() // Allow access to dogstore page for everyone
                         .requestMatchers("/register").permitAll() // Allow access to dogstore page for everyone
-
+                        .requestMatchers(HttpMethod.POST, "/*").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login") // login sivu.
@@ -37,6 +46,25 @@ public class WebSecurityConfig {
                         .logoutRequestMatcher(antMatcher("/logout"))
                         .logoutSuccessUrl("/dogstore"));
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Add your frontend origin here
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        // Enable preflight requests
+        configuration.addAllowedMethod("OPTIONS");
+        configuration.addAllowedHeader("Authorization");
+        configuration.addAllowedHeader("Content-Type");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
     @Autowired
