6aadc5f42abee69665bbb54c11c4d1f2421badca
regnfylt <148742788+regnfylt@users.noreply.github.com>
2024-05-06 10:36:24 +0300

    OrderController and OrderRepo fixes

--- src/main/java/k24/op1/dogbackend/domain/OrderRepository.java
+++ src/main/java/k24/op1/dogbackend/domain/OrderRepository.java
@@ -2,5 +2,5 @@ package k24.op1.dogbackend.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends CrudRepository<Type, Long> {
+public interface OrderRepository extends CrudRepository<Order, Long> {
 }
--- src/main/java/k24/op1/dogbackend/web/OrderController.java
+++ src/main/java/k24/op1/dogbackend/web/OrderController.java
@@ -1,4 +1,5 @@
 package k24.op1.dogbackend.web;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -6,15 +7,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import k24.op1.dogbackend.domain.Order;
+import k24.op1.dogbackend.domain.OrderRepository;
 
-@CrossOrigin(origins = "http://localhost:5173/products")
+@CrossOrigin(origins = "*")
 @RestController
 public class OrderController {
+
+    @Autowired
+    private OrderRepository orderRepository;
     
     @PostMapping("/reservations")
-    public ResponseEntity<String> createReservation(@RequestBody Order Order) {
+    // public ResponseEntity<String> createReservation(@RequestBody Order Order) {
         // Logic to save the reservation to a database or perform any other actions
-        System.out.println("Received reservation: " + Order);
-        return ResponseEntity.ok("Reservation received successfully");
+        Order createReservation(@RequestBody Order newOrder) {
+        System.out.println("Received reservation: " + newOrder);
+        return orderRepository.save(newOrder);
+      //  return ResponseEntity.ok("Reservation received successfully");
     }
+    
 }
