2e23e4ad630030fb9f6f3940e4e5bf157b31a261
Raikku <107673429+raiccu@users.noreply.github.com>
2024-05-08 12:33:04 +0300

    muutoksia muutoksia

--- src/main/java/k24/op1/dogbackend/web/OrderController.java
+++ src/main/java/k24/op1/dogbackend/web/OrderController.java
@@ -2,24 +2,31 @@ package k24.op1.dogbackend.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import k24.op1.dogbackend.domain.Order;
 import k24.op1.dogbackend.domain.OrderRepository;
 
 @CrossOrigin(origins = "*")
-@RestController
+@Controller
 public class OrderController {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @GetMapping("/orderlist")
+    public String showOrderList(Model model) {
+        model.addAttribute("orders", orderRepository.findAll());
+        return "orderlist";
+    }
     
     @PostMapping("/reservations")
     Iterable<Order> createReservations2(@RequestBody List<Order> newOrders) {
